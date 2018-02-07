d13a0c05285a035525c2076c71787aacd84f660b
lovevilla <36231697+lovevilla@users.noreply.github.com>
2018-02-07 20:59:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<title> Assignment</title>
+<body bgcolor="sky blue">
+<center><h1><i> My Personal Information</i></h1></center>
+<p><h3> Name: <u> Maria Lovevilla Gordon</u></h3></p>
+<p><h3> Address: <u> Tuyan, City of Naga</u></h3></p>
+<p><h3> Contact: <u> 09358519291</u></h3></p>
+<p><h3> Gender: <u> Female</u></h3></p>
+<p><h3> Age: <u> 26</u></h3</p>
+<p><h3> Date of Birth: <u> January 18, 1992</u></h3></p>
+<p><h3> Email Address: <u>dmlovevilla@gmail.com</u></h3></p>
+<p><h3> Civil Status: <u>Single</u></h3></p>
+
+
+
+
+
+
+
+<Center><H1><i> BIOGRAPHY </i><h1</center>
+
+
+
+
+<p><h5><i> I am the youngest of the family, i was a spoil little girl growing up.
+        Surrounded always with my masculine brothers and a very clumsy sister.
+		I finished my elementary education in Tuyan Elementary School.
+		I then continue my study in Naga National High School.
+		Now currently pursuing to finish my college education in CTU-Naga Campus</i></h5></P>
+<body>
+<html>
